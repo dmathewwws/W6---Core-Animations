920c0163b0ab3c79e631352ac4fd84ac2be18cab
Daniel Mathews <dmathewwws@Daniels-MacBook-Air.local>
2015-06-10 08:48:20 -0700

    v1

--- W6 - Bahamas Animations/ViewController.m
+++ W6 - Bahamas Animations/ViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *cloud1;
 @property (weak, nonatomic) IBOutlet UIButton *plusButton;
 @property (weak, nonatomic) IBOutlet UILabel *loginLabel;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -21,51 +22,67 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    CABasicAnimation *moveToCenter = [CABasicAnimation animation];
+    moveToCenter.keyPath = @"position.x";
+    moveToCenter.fromValue = [NSNumber numberWithFloat:-CGRectGetMidX(self.view.frame)];
+    moveToCenter.toValue = [NSNumber numberWithFloat:CGRectGetMidX(self.view.frame)];
+    moveToCenter.duration = 1.0;
+    moveToCenter.delegate = self;
     
-    self.username.center = CGPointMake(self.username.center.x - self.view.frame.size.width, self.username.center.y);
+    [self.username.layer addAnimation:moveToCenter forKey:@"usernameAppear"];
+    self.username.layer.position = CGPointMake(CGRectGetMidX(self.view.frame), self.username.center.y);
     
+    moveToCenter.beginTime = CACurrentMediaTime() + 0.5;
+    moveToCenter.fillMode = kCAFillModeBoth;
     
-    self.password.center = CGPointMake(self.username.center.x - self.view.frame.size.width, self.password.center.y);
-    
-    [UIView animateWithDuration:1.0 animations:^{
-        
-        self.username.center = CGPointMake( CGRectGetMidX(self.view.frame), self.username.center.y);
+    [self.password.layer addAnimation:moveToCenter forKey:@"passwordAppear"];
 
-        
-    } completion:^(BOOL finished) {
-        
-        [UIView animateWithDuration:1.0 delay:0.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
-            
-            self.password.center = CGPointMake( CGRectGetMidX(self.view.frame), self.password.center.y);
-            
-            
-        } completion:nil];
-        
-    }];
-    
-    self.plusButton.center = CGPointMake(self.plusButton.center.x, self.plusButton.center.y - self.view.frame.size.height);
-    
-    [UIView animateWithDuration:1.0 delay:2.0 usingSpringWithDamping:0.8 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
-        
-        self.plusButton.center = CGPointMake(self.plusButton.center.x, self.plusButton.center.y + self.view.frame.size.height);
-        
-        self.plusButton.transform = CGAffineTransformMakeRotation(M_PI_4);
-        
-    } completion:nil];
-    
-    self.loginLabel.hidden = YES;
-    
-    [UIView transitionWithView:self.loginLabel duration:2               options:UIViewAnimationOptionTransitionFlipFromBottom animations:^{
-    
-    
-        self.loginLabel.hidden = NO;
-    
-    } completion:^(BOOL finished) {
-        //
-    }];
 }
 
+- (IBAction)loginButtonPressed:(UIButton *)sender {
+    
+    CAKeyframeAnimation *shakeyShakey = [CAKeyframeAnimation animation];
+    shakeyShakey.keyPath = @"position.x";
+    shakeyShakey.values = @[ @0, @10, @-10, @10, @0 ];
+    shakeyShakey.keyTimes = @[ @0, @(2/5.0), @(3/5.0), @(4/5.0), @1 ];
+    shakeyShakey.duration = 0.4;
+    shakeyShakey.repeatCount = 1;
+    [shakeyShakey setValue:@"shakeyShakey" forKey:@"name"];
+    [shakeyShakey setValue:self.loginButton.layer forKey:@"layer"];
+    shakeyShakey.delegate = self;
+    
+    shakeyShakey.additive = YES;
+    
+    [self.password.layer addAnimation:shakeyShakey forKey:@"passwordShake"];
+    
+}
 
+-(void)animationDidStart:(CAAnimation *)anim {
+    NSLog(@"animation DID START");
+    if ([[anim valueForKey:@"name"] isEqualToString:@"shakeyShakey"]){
+        NSLog(@"inside shakeyShakey");
+    }
+}
+
+-(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    NSLog(@"animation DID STOP");
+    
+    if ([[anim valueForKey:@"name"] isEqualToString:@"shakeyShakey"]){
+        
+        CALayer *layer = [anim valueForKey:@"layer"];
+        [anim setValue:nil forKey:@"layer"];
+        
+        CABasicAnimation *pulseAnimation = [CABasicAnimation animation];
+        pulseAnimation.keyPath = @"transform.scale";
+        pulseAnimation.fromValue = @1.25;
+        pulseAnimation.toValue = @1.0;
+        pulseAnimation.duration = 0.5;
+
+        [layer addAnimation:pulseAnimation forKey:nil];
+        
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
